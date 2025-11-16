1d678074c860be4bd67d3c191ada3f1e7960c15e
devanshkant <devanshkant22@gmail.com>
2025-11-16 19:07:10 +0530

    polynomial header file modified

--- Miscellaneous/polynomial.h
+++ Miscellaneous/polynomial.h
@@ -1,108 +1,122 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
-
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
+using std::vector;
 
 template<typename T>
-class Polynomial {
-private:
-    vector<T> coefficients; // coeff[i] = coefficient of x^i
+class Polynomial{
+    private:
+        //this array holds the coefficients of polynomial
+        vector<T> coefficients;
+        int degree;
 
-    void normalize() {
-        while (coefficients.size() > 1 && coefficients.back() == T{}) {
-            coefficients.pop_back();
+        // Helper function to remove leading zeroees and update degree
+        void normalize() {
+            while (coefficients.size() > 1 && coefficients.back() == T{}) {
+                coefficients.pop_back();
+            }
+            degree = coefficients.size() - 1;
         }
-    }
-
-public:
-
-    // Default constructor
-    Polynomial() : coefficients(1, T{}) {}
-
-    // Construct from vector
-    Polynomial(const vector<T>& coeff) : coefficients(coeff) {
-        if (coefficients.empty()) coefficients.push_back(T{});
-        normalize();
-    }
-
-    // Copy constructor
-    Polynomial(const Polynomial& other) = default;
-
-    // Move constructor
-    Polynomial(Polynomial&& other) noexcept 
-        : coefficients(std::move(other.coefficients))
-    {
-        other.coefficients = { T{} };
-    }
-
-    // Copy assignment
-    Polynomial& operator=(const Polynomial& p) {
-        if (this != &p)
+    public:
+        //default constructoor
+        Polynomial(){
+            degree = 0;
+            coefficients = {0};
+        }
+        //parameterized constructor
+        Polynomial(vector<T> coeff){
+            if (coeff.empty()) {
+                coefficients = {0};
+                degree = 0;
+            } else {
+                coefficients = coeff; 
+                normalize();
+            }
+        }
+        
+        //copy constructor
+        Polynomial(const Polynomial<T> &p){
             coefficients = p.coefficients;
-        return *this;
-    }
-
-    int getDegree() const { 
-        return static_cast<int>(coefficients.size()) - 1; 
-    }
-
-    T getCoefficient(int power) const {
-        return (power < 0 || power >= coefficients.size())
-            ? T{} : coefficients[power];
-    }
-
-    void setCoefficient(int power, T value) {
-        if (power >= coefficients.size())
-            coefficients.resize(power + 1, T{});
-        coefficients[power] = value;
-        normalize();
-    }
-
-    Polynomial operator+(const Polynomial& p) const {
-        int n = max(coefficients.size(), p.coefficients.size());
-        vector<T> res(n, T{});
-
-        for (int i = 0; i < coefficients.size(); i++)
-            res[i] += coefficients[i];
-        for (int i = 0; i < p.coefficients.size(); i++)
-            res[i] += p.coefficients[i];
-
-        return Polynomial(res);
-    }
-
-    Polynomial operator-(const Polynomial& p) const {
-        int n = max(coefficients.size(), p.coefficients.size());
-        vector<T> res(n, T{});
-
-        for (int i = 0; i < coefficients.size(); i++)
-            res[i] += coefficients[i];
-        for (int i = 0; i < p.coefficients.size(); i++)
-            res[i] -= p.coefficients[i];
-
-        return Polynomial(res);
-    }
-
-    Polynomial operator*(const Polynomial& p) const {
-        int n = coefficients.size() + p.coefficients.size() - 1;
-        vector<T> res(n, T{});
-
-        for (int i = 0; i < coefficients.size(); i++)
-            for (int j = 0; j < p.coefficients.size(); j++)
-                res[i + j] += coefficients[i] * p.coefficients[j];
-
-        return Polynomial(res);
-    }
-
-    friend ostream& operator<<(ostream& os, const Polynomial& poly) {
-        for (int i = 0; i < poly.coefficients.size(); i++) {
-            os << poly.coefficients[i];
-            if (i > 0) os << "x^" << i;
-            if (i + 1 != poly.coefficients.size()) os << " + ";
+            degree = p.degree;
         }
-        return os;
-    }
-};
+        //Move Constructor
+        Polynomial(Polynomial<T> &&p) noexcept {
+            coefficients = std::move(p.coefficients);
+            degree = p.degree;
+            p.degree = 0;
+            p.coefficients.clear();
+        }
+        //over load assignemment operator =
+        Polynomial& operator=(const Polynomial<T> &p){
+            //if both of them are exact same, then thhere cannot be assignment, we return *this only
+            if(this != &p){
+                coefficients = p.coefficients;
+                degree = p.degree;
+            }
+            return *this;
+        }
+        
+        T getCoefficient(int power) const{
+            if(power < 0 || power > degree){
+                return 0;   //out of bounds ke liye zero 
+            }
+            return coefficients[power];
+        }
+        //get dgree
+        int getDegree() const{
+            return degree;
+        }
+        T getConstant() const {
+            if (coefficients.empty()) return 0;       // ⭐ FIX: safety check for empty vector
+            return coefficients[0];
+        }
+        void setCoefficient(int power, T val){
+            if (power < 0) return; 
+            if (power >= coefficients.size()) {
+                coefficients.resize(power + 1, T{});
+            }
+            coefficients[power] = val;
+            normalize();
+        }
+        
+        //overload +, -, * for polynomial arithmetic
+        Polynomial operator+(const Polynomial &p) const {
+            int n = std::max(this->coefficients.size(), p.coefficients.size());
+            vector<T> res(n, T{});
+            
+            for(int i = 0; i < this->coefficients.size(); ++i){
+                res[i] += this->coefficients[i];
+            }
+            for(int i = 0; i < p.coefficients.size(); ++i){
+                res[i] += p.coefficients[i];
+            }
+            return Polynomial(res);
+        }
+        Polynomial operator- (const Polynomial<T> &p) const{
+            int n = std::max(p.coefficients.size(), this->coefficients.size());
+            vector<T> res(n, T{});
+            for(int i = 0; i < this->coefficients.size(); ++i){
+                res[i] += this->coefficients[i];
+            }
+            for(int i = 0; i < p.coefficients.size(); ++i){
+                res[i] -= p.coefficients[i];
+            }
+            
+            return Polynomial(res);
+        }
+        Polynomial operator*(const Polynomial& p) const {
+            int n1 = coefficients.size();
+            int n2 = p.coefficients.size();
+            std::vector<T> res(n1 + n2 - 1, T{});
 
+            for (int i = 0; i < n1; i++)
+                for (int j = 0; j < n2; j++)
+                    res[i + j] += coefficients[i] * p.coefficients[j];
+
+            return Polynomial(res);
+        }
+        
+};
 #endif
