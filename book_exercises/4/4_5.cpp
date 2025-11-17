5b8c978fbdb19320e3e94bc3ee6c463447c45e4d
Vaibhav Yadav <vaibhav20y@gmail.com>
2025-11-17 09:25:19 +0530

    Added Questions to the chapter 4 exercises

--- book_exercises/4/4_5.cpp
+++ book_exercises/4/4_5.cpp
@@ -1,3 +1,12 @@
+/*
+	Question 4.5
+	Create a structure of type date that contains three members: the month, the day of the
+	month, and the year, all of type int. (Or use day-month-year order if you prefer.) Have
+	the user enter a date in the format 12/31/2001, store it in a variable of type struct date,
+	then retrieve the values from the variable and print them out in the same format.
+*/
+
+
 #include<iostream>
 #include<string>
 using namespace std;
